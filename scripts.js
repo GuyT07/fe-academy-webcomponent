7392ed240ff876e055581d6a012d46d9dae5f36a
JasperV <1297464+JasperV@users.noreply.github.com>
2019-06-13 14:09:12 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,117 +1,115 @@
-// Create a class for the element
 class NASAArticle extends HTMLElement {
+  #root
+  #container
+  #dateElement
+  #explanationElement
+  #imageElement
 
-    attributes = undefined;
-    imageElement = undefined;
+  #attributes = new Map
 
-    initAttributes() {
-        const defaultValues = {
-            date: '01-01-1970',
-            explanation: 'Fill in your explanation',
-            hdurl: '',
-            media_type: '',
-            service_version: '',
-            title: 'Title',
-            url: 'Url'
-        };
-        for (let prop in defaultValues) {
-            let value = this.getAttribute(prop);
-            defaultValues[prop] = (value && value.trim()) ? value : defaultValues[prop];
-        }
-        return defaultValues;
+  static #defaults = new Map( [
+    [ 'date'            , '01-01-1970'                    ],
+    [ 'explanation'     , 'Fill in your explanation'      ],
+    [ 'sdurl'           , 'https://picsum.photos/320/240' ],
+    [ 'hdurl'           , ''                              ],
+    [ 'media_type'      , ''                              ],
+    [ 'service_version' , ''                              ],
+    [ 'title'           , 'Title'                         ],
+    [ 'url'             , 'Url'                           ],
+  ] )
+
+  initAttributes() {
+    for ( const attr of NASAArticle.#defaults.keys() ) {
+      const val = this.getAttribute( attr )
+
+      this.#attributes.set(
+        attr,
+        ( val && val.trim() ) || NASAArticle.#defaults.get( attr )
+      )
     }
 
-    constructor() {
-        // Always call super first in constructor
-        super();
+    this.#attributes = Object.fromEntries( this.#attributes.entries() )
 
-        // Create a shadow root
-        const shadow = this.attachShadow({mode: 'open'});
+    return this
+  }
 
-        this.attributes = this.initAttributes();
+  buildDom() {
+    let c, d, e, i
 
-        // Create spans
-        const container = document.createElement('div');
-        container.setAttribute('class', 'container');
+    this.#container           = c = document.createElement( 'div' )
+    this.#dateElement         = d = document.createElement( 'span'  )
+    this.#explanationElement  = e = document.createElement( 'div' )
+    this.#imageElement        = i = document.createElement( 'img' )
 
-        const dateElement = document.createElement('span');
-        dateElement.setAttribute('class', 'date');
-        dateElement.innerText = `${this.attributes.date} - ${this.attributes.title}`;
+    c.setAttribute( 'class', 'container' )
+    d.setAttribute( 'class', 'date' )
+    e.setAttribute( 'class', 'explanation' )
+    i.setAttribute( 'class', 'image' )
 
-        container.appendChild(dateElement);
+    c.appendChild( d )
+    c.appendChild( e )
+    c.appendChild( i )
+    this.#root.appendChild( c )
 
-        const explanationElement = document.createElement('div');
-        explanationElement.setAttribute('class', 'explanation');
-        explanationElement.innerText = this.attributes.explanation;
+    return this
+  }
 
-        container.appendChild(explanationElement);
+  buildStyles() {
+    const style = document.createElement( 'style' )
 
-        this.imageElement = document.createElement('img');
-        this.imageElement.setAttribute('class', 'image');
-        this.imageElement.setAttribute('src', this.attributes.hdurl);
+    style.textContent = `.date { font-size: 1em; display: inline-block; margin-bottom: 20px; }
+.image { max-width: 100%; }`
 
-        container.appendChild(this.imageElement);
+    this.#root.appendChild( style )
 
-        this.setHdUrl();
+    return this
+  }
 
-        // Create some CSS to apply to the shadow dom
-        const style = document.createElement('style');
+  loadData() {
+    this.#dateElement.innerText         = `${this.#attributes.date} - ${this.#attributes.title}`
+    this.#explanationElement.innerText  = this.#attributes.explanation
 
-        style.textContent = `
-            .date {
-              font-size: 1em;
-              display: inline-block;
-              margin-bottom: 20px;
+    this.#imageElement.setAttribute( 'src', this.#attributes.sdurl )
+
+    this.getHdImage()
+
+    return this
+  }
+
+  constructor() {
+    super();
+
+    this.#root = this.attachShadow( { mode: 'open' } )
+
+    this.initAttributes().buildDom().buildStyles().loadData()
+  }
+
+  getHdImage() {
+    return fetch( this.#attributes.hdurl )
+      .then( res => {
+        const reader = res.body.getReader()
+
+        return new ReadableStream( {
+          async start( controller ) {
+            while ( true ) {
+              const { done, value } = await reader.read()
+
+              if ( done ) break
+
+              controller.enqueue( value )
             }
-    
-            .image {
-              max-width: 100%;
-            }`;
 
-        // Attach the created elements to the shadow dom
-        shadow.appendChild(style);
-        console.log(style.isConnected);
-        shadow.appendChild(container);
-    }
-
-    setHdUrl() {
-        return getImageBlobUrl()
-            .then(result => this.imageElement.setAttribute('src', result));
-    }
+            controller.close()
+            reader.releaseLock()
+          }
+        } )
+      } )
+      .then( res => new Response( res ) )
+      .then( res => res.blob() )
+      .then( blob => URL.createObjectURL( blob ) )
+      .then( url => this.#imageElement.setAttribute( 'src', url ) )
+      .catch( console.error )
+  }
 }
 
-// Define the new element
-customElements.define('nasa-article', NASAArticle);
-
-async function getImageBlobUrl() {
-    return fetch('https://picsum.photos/4951/3301')
-        .then(response => {
-            const reader = response.body.getReader();
-
-            return new ReadableStream({
-                async start(controller) {
-                    while (true) {
-                        const {done, value} = await reader.read();
-                        // When no more data needs to be consumed, break the reading
-                        if (done) {
-                            break;
-                        }
-                        // Enqueue the next data chunk into our target stream
-                        controller.enqueue(value);
-                    }
-                    // Close the stream
-                    controller.close();
-                    reader.releaseLock();
-                }
-            })
-        })
-        // Create a new response out of the stream
-        .then(rs => new Response(rs))
-        // Create an object URL for the response
-        .then(response => response.blob())
-        .then(blob => URL.createObjectURL(blob))
-    // Update image
-    //.then(url => image.src = url)
-    // .then(console.log)
-    // .catch(console.error)
-}
+customElements.define( 'nasa-article', NASAArticle )
